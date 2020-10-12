808a669d956e10a5a01f0075f9e7f98d129fd2b9
putueka_id <72745823+putuekacode@users.noreply.github.com>
2020-10-12 21:11:20 +0800

    Plus-Minus-Multiplied (Pascal)
    (num1 + num2) * num3

---
+++ PlusMinusMultiplied.pas
@@ -0,0 +1,16 @@
+program PlusMinusMultiplied; 
+{But I Remember, Everything}
+
+var
+  num1, num2, num3, sum: Integer;
+
+
+begin
+  Write('Enter The First  Number: ');
+  ReadLn(num1);
+  Write('Enter The Second Number: ');
+  ReadLn(num2);
+  WriteLn();
+  Write('The Number You Enter Has A Value Of : ', num1 + num2);
+  ReadLn();
+end.
