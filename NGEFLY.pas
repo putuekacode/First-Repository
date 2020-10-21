f489299b1200365e30cc9390f1f8ea66be49cbe7
putueka_id <72745823+putuekacode@users.noreply.github.com>
2020-10-21 10:52:23 +0800

    Flying ;)
    Wth am i even making?

---
+++ NGEFLY.pas
@@ -0,0 +1,909 @@
+program NGEFLY; 
+
+uses
+  crt;
+
+
+var
+  x: Integer;
+
+
+begin
+  x := 1;
+  while x < 10 do 
+    begin
+      WriteLn(x);
+      x := x + 1;
+      ReadLn();
+    end;
+  if x >= 9 then
+    begin
+      while x >= 9 do 
+        begin
+          WriteLn(x);
+          x := x - 1;
+          ReadLn();
+        end;
+    end ;
+  if x <= 11 then
+    begin
+      while x <= 11 do 
+        begin
+          WriteLn(x);
+          x := x + 1;
+          ReadLn();
+        end;
+    end ;
+  begin
+    WriteLn(x);
+    x := x + 1;
+    ReadLn();
+  end;
+  ReadLn();
+  if x >= 12 then
+    begin
+      WriteLn('Noice');
+    end ;
+  ReadLn();
+  repeat 
+    x := 1;
+    begin
+      WriteLn(x);
+      x := x - 1;
+    end;
+  until x = 0;
+  x := 0;
+  if x < 10 then
+    begin
+      while x < 10 do 
+        begin
+          WriteLn(x);
+          x := x + 1;
+          ReadLn();
+        end;
+    end ;
+  begin
+    x := 1;
+    while x < 11 do 
+      begin
+        WriteLn(x);
+        x := x + 1;
+      end;
+    ReadLn();
+    repeat 
+      WriteLn(x);
+      x := x - 1;
+    until x = 0;
+    if x <= 1 then
+      begin
+        while x < 11 do 
+          begin
+            WriteLn(x);
+            x := x + 1;
+            ReadLn();
+          end;
+        readln;
+      end ;
+    begin
+      x := 1;
+      while x < 10 do 
+        begin
+          WriteLn(x);
+          x := x + 1;
+          ReadLn();
+        end;
+      if x >= 9 then
+        begin
+          while x >= 9 do 
+            begin
+              WriteLn(x);
+              x := x - 1;
+              ReadLn();
+            end;
+        end ;
+    end;
+  end;
+  if x < 10 then
+    begin
+      while x < 10 do 
+        begin
+          WriteLn(x);
+          x := x + 1;
+          ReadLn();
+        end;
+    end ;
+  begin
+    x := 1;
+    while x < 11 do 
+      begin
+        WriteLn(x);
+        x := x + 1;
+      end;
+    ReadLn();
+    repeat 
+      WriteLn(x);
+      x := x - 1;
+    until x = 0;
+    if x <= 1 then
+      begin
+        while x < 11 do 
+          begin
+            WriteLn(x);
+            x := x + 1;
+            ReadLn();
+          end;
+        readln;
+      end ;
+  end;
+  begin
+    x := 1;
+    while x < 10 do 
+      begin
+        WriteLn(x);
+        x := x + 1;
+        ReadLn();
+      end;
+    if x >= 9 then
+      begin
+        while x >= 9 do 
+          begin
+            WriteLn(x);
+            x := x - 1;
+            ReadLn();
+          end;
+      end ;
+    if x <= 11 then
+      begin
+        while x <= 11 do 
+          begin
+            WriteLn(x);
+            x := x + 1;
+            ReadLn();
+          end;
+      end ;
+    begin
+      WriteLn(x);
+      x := x + 1;
+      ReadLn();
+    end;
+    ReadLn();
+    if x >= 12 then
+      begin
+        WriteLn('Noice');
+      end ;
+    ReadLn();
+    repeat 
+      x := 1;
+      begin
+        WriteLn(x);
+        x := x - 1;
+      end;
+    until x = 0;
+    x := 0;
+    if x < 10 then
+      begin
+        while x < 10 do 
+          begin
+            WriteLn(x);
+            x := x + 1;
+            ReadLn();
+          end;
+      end ;
+    begin
+      x := 1;
+      while x < 11 do 
+        begin
+          WriteLn(x);
+          x := x + 1;
+        end;
+      ReadLn();
+      repeat 
+        WriteLn(x);
+        x := x - 1;
+      until x = 0;
+      if x <= 1 then
+        begin
+          while x < 11 do 
+            begin
+              WriteLn(x);
+              x := x + 1;
+              ReadLn();
+            end;
+          readln;
+        end ;
+      begin
+        x := 1;
+        while x < 10 do 
+          begin
+            WriteLn(x);
+            x := x + 1;
+            ReadLn();
+          end;
+        if x >= 9 then
+          begin
+            while x >= 9 do 
+              begin
+                WriteLn(x);
+                x := x - 1;
+                ReadLn();
+              end;
+          end ;
+      end;
+    end;
+    if x < 10 then
+      begin
+        while x < 10 do 
+          begin
+            WriteLn(x);
+            x := x + 1;
+            ReadLn();
+          end;
+      end ;
+    begin
+      x := 1;
+      while x < 11 do 
+        begin
+          WriteLn(x);
+          x := x + 1;
+        end;
+      ReadLn();
+      repeat 
+        WriteLn(x);
+        x := x - 1;
+      until x = 0;
+      if x <= 1 then
+        begin
+          while x < 11 do 
+            begin
+              WriteLn(x);
+              x := x + 1;
+              ReadLn();
+            end;
+          readln;
+        end ;
+    end;
+  end;
+  begin
+    x := 1;
+    while x < 10 do 
+      begin
+        WriteLn(x);
+        x := x + 1;
+        ReadLn();
+      end;
+    if x >= 9 then
+      begin
+        while x >= 9 do 
+          begin
+            WriteLn(x);
+            x := x - 1;
+            ReadLn();
+          end;
+      end ;
+    if x <= 11 then
+      begin
+        while x <= 11 do 
+          begin
+            WriteLn(x);
+            x := x + 1;
+            ReadLn();
+          end;
+      end ;
+    begin
+      WriteLn(x);
+      x := x + 1;
+      ReadLn();
+    end;
+    ReadLn();
+    if x >= 12 then
+      begin
+        WriteLn('Noice');
+      end ;
+    ReadLn();
+    repeat 
+      x := 1;
+      begin
+        WriteLn(x);
+        x := x - 1;
+      end;
+    until x = 0;
+    x := 0;
+    if x < 10 then
+      begin
+        while x < 10 do 
+          begin
+            WriteLn(x);
+            x := x + 1;
+            ReadLn();
+          end;
+      end ;
+    begin
+      x := 1;
+      while x < 11 do 
+        begin
+          WriteLn(x);
+          x := x + 1;
+        end;
+      ReadLn();
+      repeat 
+        WriteLn(x);
+        x := x - 1;
+      until x = 0;
+      if x <= 1 then
+        begin
+          while x < 11 do 
+            begin
+              WriteLn(x);
+              x := x + 1;
+              ReadLn();
+            end;
+          readln;
+        end ;
+      begin
+        x := 1;
+        while x < 10 do 
+          begin
+            WriteLn(x);
+            x := x + 1;
+            ReadLn();
+          end;
+        if x >= 9 then
+          begin
+            while x >= 9 do 
+              begin
+                WriteLn(x);
+                x := x - 1;
+                ReadLn();
+              end;
+          end ;
+      end;
+    end;
+    if x < 10 then
+      begin
+        while x < 10 do 
+          begin
+            WriteLn(x);
+            x := x + 1;
+            ReadLn();
+          end;
+      end ;
+    begin
+      x := 1;
+      while x < 11 do 
+        begin
+          WriteLn(x);
+          x := x + 1;
+        end;
+      ReadLn();
+      repeat 
+        WriteLn(x);
+        x := x - 1;
+      until x = 0;
+      if x <= 1 then
+        begin
+          while x < 11 do 
+            begin
+              WriteLn(x);
+              x := x + 1;
+              ReadLn();
+            end;
+          readln;
+        end ;
+    end;
+  end;
+  begin
+    x := 1;
+    while x < 10 do 
+      begin
+        WriteLn(x);
+        x := x + 1;
+        ReadLn();
+      end;
+    if x >= 9 then
+      begin
+        while x >= 9 do 
+          begin
+            WriteLn(x);
+            x := x - 1;
+            ReadLn();
+          end;
+      end ;
+    if x <= 11 then
+      begin
+        while x <= 11 do 
+          begin
+            WriteLn(x);
+            x := x + 1;
+            ReadLn();
+          end;
+      end ;
+    begin
+      WriteLn(x);
+      x := x + 1;
+      ReadLn();
+    end;
+    ReadLn();
+    if x >= 12 then
+      begin
+        WriteLn('Noice');
+      end ;
+    ReadLn();
+    repeat 
+      x := 1;
+      begin
+        WriteLn(x);
+        x := x - 1;
+      end;
+    until x = 0;
+    x := 0;
+    if x < 10 then
+      begin
+        while x < 10 do 
+          begin
+            WriteLn(x);
+            x := x + 1;
+            ReadLn();
+          end;
+      end ;
+    begin
+      x := 1;
+      while x < 11 do 
+        begin
+          WriteLn(x);
+          x := x + 1;
+        end;
+      ReadLn();
+      repeat 
+        WriteLn(x);
+        x := x - 1;
+      until x = 0;
+      if x <= 1 then
+        begin
+          while x < 11 do 
+            begin
+              WriteLn(x);
+              x := x + 1;
+              ReadLn();
+            end;
+          readln;
+        end ;
+      begin
+        x := 1;
+        while x < 10 do 
+          begin
+            WriteLn(x);
+            x := x + 1;
+            ReadLn();
+          end;
+        if x >= 9 then
+          begin
+            while x >= 9 do 
+              begin
+                WriteLn(x);
+                x := x - 1;
+                ReadLn();
+              end;
+          end ;
+      end;
+    end;
+    if x < 10 then
+      begin
+        while x < 10 do 
+          begin
+            WriteLn(x);
+            x := x + 1;
+            ReadLn();
+          end;
+      end ;
+    begin
+      x := 1;
+      while x < 11 do 
+        begin
+          WriteLn(x);
+          x := x + 1;
+        end;
+      ReadLn();
+      repeat 
+        WriteLn(x);
+        x := x - 1;
+      until x = 0;
+      if x <= 1 then
+        begin
+          while x < 11 do 
+            begin
+              WriteLn(x);
+              x := x + 1;
+              ReadLn();
+            end;
+          readln;
+        end ;
+    end;
+  end;
+  begin
+  x := 1;
+  while x < 10 do 
+    begin
+      WriteLn(x);
+      x := x + 1;
+      ReadLn();
+    end;
+  if x >= 9 then
+    begin
+      while x >= 9 do 
+        begin
+          WriteLn(x);
+          x := x - 1;
+          ReadLn();
+        end;
+    end ;
+  if x <= 11 then
+    begin
+      while x <= 11 do 
+        begin
+          WriteLn(x);
+          x := x + 1;
+          ReadLn();
+        end;
+    end ;
+  begin
+    WriteLn(x);
+    x := x + 1;
+    ReadLn();
+  end;
+  ReadLn();
+  if x >= 12 then
+    begin
+      WriteLn('Noice');
+    end ;
+  ReadLn();
+  repeat 
+    x := 1;
+    begin
+      WriteLn(x);
+      x := x - 1;
+    end;
+  until x = 0;
+  x := 0;
+  if x < 10 then
+    begin
+      while x < 10 do 
+        begin
+          WriteLn(x);
+          x := x + 1;
+          ReadLn();
+        end;
+    end ;
+  begin
+    x := 1;
+    while x < 11 do 
+      begin
+        WriteLn(x);
+        x := x + 1;
+      end;
+    ReadLn();
+    repeat 
+      WriteLn(x);
+      x := x - 1;
+    until x = 0;
+    if x <= 1 then
+      begin
+        while x < 11 do 
+          begin
+            WriteLn(x);
+            x := x + 1;
+            ReadLn();
+          end;
+        readln;
+      end ;
+    begin
+      x := 1;
+      while x < 10 do 
+        begin
+          WriteLn(x);
+          x := x + 1;
+          ReadLn();
+        end;
+      if x >= 9 then
+        begin
+          while x >= 9 do 
+            begin
+              WriteLn(x);
+              x := x - 1;
+              ReadLn();
+            end;
+        end ;
+    end;
+  end;
+  if x < 10 then
+    begin
+      while x < 10 do 
+        begin
+          WriteLn(x);
+          x := x + 1;
+          ReadLn();
+        end;
+    end ;
+  begin
+    x := 1;
+    while x < 11 do 
+      begin
+        WriteLn(x);
+        x := x + 1;
+      end;
+    ReadLn();
+    repeat 
+      WriteLn(x);
+      x := x - 1;
+    until x = 0;
+    if x <= 1 then
+      begin
+        while x < 11 do 
+          begin
+            WriteLn(x);
+            x := x + 1;
+            ReadLn();
+          end;
+        readln;
+      end ;
+  end;
+  end;
+  begin
+  x := 1;
+  while x < 10 do 
+    begin
+      WriteLn(x);
+      x := x + 1;
+      ReadLn();
+    end;
+  if x >= 9 then
+    begin
+      while x >= 9 do 
+        begin
+          WriteLn(x);
+          x := x - 1;
+          ReadLn();
+        end;
+    end ;
+  if x <= 11 then
+    begin
+      while x <= 11 do 
+        begin
+          WriteLn(x);
+          x := x + 1;
+          ReadLn();
+        end;
+    end ;
+  begin
+    WriteLn(x);
+    x := x + 1;
+    ReadLn();
+  end;
+  ReadLn();
+  if x >= 12 then
+    begin
+      WriteLn('Noice');
+    end ;
+  ReadLn();
+  repeat 
+    x := 1;
+    begin
+      WriteLn(x);
+      x := x - 1;
+    end;
+  until x = 0;
+  x := 0;
+  if x < 10 then
+    begin
+      while x < 10 do 
+        begin
+          WriteLn(x);
+          x := x + 1;
+          ReadLn();
+        end;
+    end ;
+  begin
+    x := 1;
+    while x < 11 do 
+      begin
+        WriteLn(x);
+        x := x + 1;
+      end;
+    ReadLn();
+    repeat 
+      WriteLn(x);
+      x := x - 1;
+    until x = 0;
+    if x <= 1 then
+      begin
+        while x < 11 do 
+          begin
+            WriteLn(x);
+            x := x + 1;
+            ReadLn();
+          end;
+        readln;
+      end ;
+    begin
+      x := 1;
+      while x < 10 do 
+        begin
+          WriteLn(x);
+          x := x + 1;
+          ReadLn();
+        end;
+      if x >= 9 then
+        begin
+          while x >= 9 do 
+            begin
+              WriteLn(x);
+              x := x - 1;
+              ReadLn();
+            end;
+        end ;
+    end;
+  end;
+  if x < 10 then
+    begin
+      while x < 10 do 
+        begin
+          WriteLn(x);
+          x := x + 1;
+          ReadLn();
+        end;
+    end ;
+  begin
+    x := 1;
+    while x < 11 do 
+      begin
+        WriteLn(x);
+        x := x + 1;
+      end;
+    ReadLn();
+    repeat 
+      WriteLn(x);
+      x := x - 1;
+    until x = 0;
+    if x <= 1 then
+      begin
+        while x < 11 do 
+          begin
+            WriteLn(x);
+            x := x + 1;
+            ReadLn();
+          end;
+        readln;
+      end ;
+  end;
+  end;
+  begin
+  x := 1;
+  while x < 10 do 
+    begin
+      WriteLn(x);
+      x := x + 1;
+      ReadLn();
+    end;
+  if x >= 9 then
+    begin
+      while x >= 9 do 
+        begin
+          WriteLn(x);
+          x := x - 1;
+          ReadLn();
+        end;
+    end ;
+  if x <= 11 then
+    begin
+      while x <= 11 do 
+        begin
+          WriteLn(x);
+          x := x + 1;
+          ReadLn();
+        end;
+    end ;
+  begin
+    WriteLn(x);
+    x := x + 1;
+    ReadLn();
+  end;
+  ReadLn();
+  if x >= 12 then
+    begin
+      WriteLn('Noice');
+    end ;
+  ReadLn();
+  repeat 
+    x := 1;
+    begin
+      WriteLn(x);
+      x := x - 1;
+    end;
+  until x = 0;
+  x := 0;
+  if x < 10 then
+    begin
+      while x < 10 do 
+        begin
+          WriteLn(x);
+          x := x + 1;
+          ReadLn();
+        end;
+    end ;
+  begin
+    x := 1;
+    while x < 11 do 
+      begin
+        WriteLn(x);
+        x := x + 1;
+      end;
+    ReadLn();
+    repeat 
+      WriteLn(x);
+      x := x - 1;
+    until x = 0;
+    if x <= 1 then
+      begin
+        while x < 11 do 
+          begin
+            WriteLn(x);
+            x := x + 1;
+            ReadLn();
+          end;
+        readln;
+      end ;
+    begin
+      x := 1;
+      while x < 10 do 
+        begin
+          WriteLn(x);
+          x := x + 1;
+          ReadLn();
+        end;
+      if x >= 9 then
+        begin
+          while x >= 9 do 
+            begin
+              WriteLn(x);
+              x := x - 1;
+              ReadLn();
+            end;
+        end ;
+    end;
+  end;
+  if x < 10 then
+    begin
+      while x < 10 do 
+        begin
+          WriteLn(x);
+          x := x + 1;
+          ReadLn();
+        end;
+    end ;
+  begin
+    x := 1;
+    while x < 11 do 
+      begin
+        WriteLn(x);
+        x := x + 1;
+      end;
+    ReadLn();
+    repeat 
+      WriteLn(x);
+      x := x - 1;
+    until x = 0;
+    if x <= 1 then
+      begin
+        while x < 11 do 
+          begin
+            WriteLn(x);
+            x := x + 1;
+            ReadLn();
+          end;
+        readln;
+      end ;
+  end;
+  end;
+  {FUCKKKKK SUSAH SEKALI BIAR BISA KEULANG TERUS
+SAAT SI USER MENGETIK ENTER}
+  
+end.
